5f4ed74387d205e973ee4cc767745ec32addc449
yerinsky <aaassddf3688@gmail.com>
2023-09-20 20:41:46 +0900

    model module

--- coord_rgb/modules/model.py
+++ coord_rgb/modules/model.py
@@ -1,3 +1,33 @@
 import torch.nn as nn
-from img_dataset import img_flatten, xy_flatten
 
+
+# mlp
+class MLP(nn.Module):
+    def __init__(self, in_features, hidden_features, out_features):
+        super().__init__()
+
+        # self.flatten = nn.Flatten()
+
+        self.neural_net = nn.Sequential(
+            
+            # layer 1
+            nn.Linear(in_features, hidden_features),
+            nn.ReLU(),
+            nn.Dropout(p = 0.5),
+
+            # layer 2
+            nn.Linear(hidden_features, hidden_features),
+            nn.ReLU(),
+            nn.Dropout(p = 0.5),
+
+            # layer 3
+            nn.Linear(hidden_features, out_features),
+            nn.Tanh()
+
+        )
+
+    def forward(self, x):
+        return self.neural_net(x)
+
+# declare network
+net = MLP(in_features=2, hidden_features=500, out_features=3)
