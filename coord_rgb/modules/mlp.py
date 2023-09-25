5886fb67de1119bca17dc401ac74cb4cc488faa4
yerinsky <aaassddf3688@gmail.com>
2023-09-25 17:57:29 +0900

    xy to rgb with positional encoding

--- coord_rgb/modules/mlp.py
+++ coord_rgb/modules/mlp.py
@@ -13,23 +13,19 @@ class MLP(nn.Module):
                 # nn.Dropout(p=0.5)
         ]
 
-        # hidden layer
-        hidden_layer_list = [
-            nn.Linear(hidden_feature, hidden_feature),
-            nn.BatchNorm1d(hidden_feature),
-            nn.ReLU(),
-            # nn.Dropout(p = 0.5)
-        ]
-
         # last layer
         out_layer_list = [
             nn.Linear(hidden_feature, out_feature),
             nn.Sigmoid()
         ]
 
-
+        # hidden layers
         for _ in range(hidden_layers):
-            layers.extend(hidden_layer_list)
+            layers.extend([
+                nn.Linear(hidden_feature, hidden_feature),
+                nn.BatchNorm1d(hidden_feature),
+                nn.ReLU()
+            ])
         
         layers.extend(out_layer_list)
 
@@ -38,44 +34,16 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.net(x)
 
-class MLP2(torch.nn.Module):
-    def __init__(
-        self,
-        in_features: int,
-        hidden_features: int,
-        hidden_layers: int,
-        out_features: int
-    ):
-        super().__init__()
-        # out_features = out_features if out_features is not None else hidden_features
-        layers = [
-            torch.nn.Linear(in_features, hidden_features),
-            torch.nn.BatchNorm1d(hidden_features),  # Batch Normalization layer
-            torch.nn.ReLU(),
-        ]
-        for _ in range(hidden_layers):
-            layers.extend([
-                torch.nn.Linear(hidden_features, hidden_features),
-                torch.nn.BatchNorm1d(hidden_features),  # Batch Normalization layer
-                torch.nn.ReLU(),
-            ])
-        layers.append(torch.nn.Linear(hidden_features, out_features))
-        layers.append(torch.nn.Sigmoid())  # Assuming you want to keep the Sigmoid on the output layer
-
-        self.net = torch.nn.Sequential(*layers)
-    
-    def forward(self, x: torch.Tensor):
-        return self.net(x)
 
 # declare network
 # net = MLP(in_feature=2, hidden_feature=32, hidden_layers=8, out_feature=3)
-net_2 = MLP2(in_features=2, hidden_features=32, hidden_layers=8, out_features=3)
+
 
 # print(f'net: {net}')
-# print(f'net: {net_2}')
+
 
 
 # model = net
-model = net_2
+
 
 
