95330df1643dd64225cc7d4da547e4b5c74f55b9
yerinsky <aaassddf3688@gmail.com>
2023-10-06 13:45:33 +0900

    time heatmap

--- src/heatmap/heatmap_time.py
+++ src/heatmap/heatmap_time.py
@@ -12,6 +12,8 @@ from PIL import Image
 import seaborn as sns
 import pandas as pd
 import os
+import time
+
 
 
 def main():
@@ -19,16 +21,15 @@ def main():
     # params 
     learning_rate = 5e-3
     num_epochs = 300
-    max_pixel = 1.0
 
     # create layer and neuron list
-    neuron_list = [16, 32, 64, 128, 256, 512]
-    hidden_layer_list = [1, 2, 4, 6, 8, 10]
-    # neuron_list = [128]
-    # hidden_layer_list = [8]
-
-    # get images
-    img_df, crop_size = table_images.make_table()
+    # neuron_list = [16, 32, 64, 128, 256, 512]
+    # hidden_layer_list = [1, 2, 4, 6, 8, 10]
+    neuron_list = [16, 32]
+    hidden_layer_list = [1, 2]
+    
+    # get imagesㄴ
+    img_df, _ = table_images.make_table()
 
     # get a image
     for image in range(1, len(img_df.index) + 1):
@@ -51,11 +52,14 @@ def main():
         criterion = nn.MSELoss()
 
         # initialize psnr_list
-        psnr_list = np.zeros((len(neuron_list), len(hidden_layer_list)))
+        train_time_list = np.zeros((len(neuron_list), len(hidden_layer_list)))
 
         # train from num_neuron * num_hidden_layer
         for layer in range(len(hidden_layer_list)):
             for neuron in range(len(neuron_list)):
+
+                # start time
+                start_time = time.time()
 
                 model = MLP(in_feature=256, hidden_feature=neuron_list[neuron], hidden_layers=hidden_layer_list[layer], out_feature=3)
 
@@ -71,38 +75,32 @@ def main():
                     loss.backward()
                     optimizer.step()
 
-                    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
+                    print(f"Image{image}, Epoch {epoch+1}/{num_epochs}, layer: {hidden_layer_list[layer]}, neuron: {neuron_list[neuron]}, Loss: {loss.item()}")
+        
+                # end time
+                end_time = time.time()
 
-                # calc psnr
-                calc_psnr = 10 * torch.log10(max_pixel ** 2 / loss)
-                psnr_list[layer, neuron] = calc_psnr
+                # calc train time
+                train_time = end_time - start_time
 
-                print(f"layer: {hidden_layer_list[layer]}, neuron: {neuron_list[neuron]}, Loss: {loss.item()}, calc_psnr: {calc_psnr}")
-
-                # Getting the output from the network and reshaping it
-                generated_img = model(fourier_result)
-                generated_reshape = torch.reshape(generated_img, (crop_size, crop_size, 3))
-
-                generated_reshape = generated_reshape * 255.0
-                generated_reshape = generated_reshape.detach().numpy()
-
-                # save image
-                img_path = os.path.join(dir_path + img_dir, f'reconstructed_image_{hidden_layer_list[layer]}_{neuron_list[neuron]}.jpg')
-                save_img = Image.fromarray(generated_reshape.astype(np.uint8))
-                save_img.save(img_path)
-
+                print(f'Image {image} train time: {train_time}')
+                train_time_list[neuron, layer] = train_time
+        
         # make table
-        hidden_layer_df = pd.DataFrame(psnr_list, index=hidden_layer_list, columns=neuron_list)
+        train_time_df = pd.DataFrame(train_time_list, index=hidden_layer_list, columns=neuron_list)
+        # print(train_time_df)
 
         # generate heatmap and save
         plt.figure(figsize=(15, 8))
-        heat_map = sns.heatmap(hidden_layer_df, cmap='GnBu', xticklabels=True, yticklabels=True)
+        heat_map = sns.heatmap(train_time_df, cmap='GnBu', annot = True, annot_kws={"size": 10}, xticklabels=True, yticklabels=True)
         plt.xlabel('neurons')
         plt.ylabel('layers')
-        plt.title('heatmap_img' + str(image))
+        plt.title('time_heatmap_img' + str(image))
 
-        heatmap_image_path = os.path.join(dir_path + img_dir, f'heatmap_image_{image}.jpg')
+        heatmap_image_path = os.path.join(dir_path + img_dir, f'time_heatmap_image_{image}.jpg')
         heat_map.figure.savefig(heatmap_image_path)
+
+
 
 if __name__ == '__main__':
     main()
